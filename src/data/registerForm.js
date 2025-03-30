7701b695481dae5ff09e87c62c58502ca0115aa1
Alexander Hom <alexanderhom19@gmail.com>
2025-03-30 16:30:26 -0700

    Add register component

---
+++ src/data/registerForm.js
@@ -0,0 +1,34 @@
+export const registerForm = {
+  fields: [
+    {
+      name: "firstName",
+      label: "First name",
+      type: "text",
+    },
+    {
+      name: "lastName",
+      label: "Last name",
+      type: "text",
+    },
+    {
+      name: "email",
+      label: "Email",
+      type: "text",
+    },
+    {
+      name: "username",
+      label: "Username",
+      type: "text",
+    },
+    {
+      name: "password",
+      label: "Password",
+      type: "password",
+    },
+    {
+      name: "passwordConfirmation",
+      label: "Confirm password",
+      type: "password",
+    },
+  ],
+};
---
+++ src/pages/Register.jsx
@@ -0,0 +1,72 @@
+import { Box, Button, Card, Field, Input, Stack, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import LabelledInputField from "../components/LabelledInputField";
+import { Link } from "react-router-dom";
+import { registerForm } from "../data/registerForm";
+
+function Register() {
+  const [inputData, setInputData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    username: "",
+    password: "",
+    passwordConfirmation: "",
+  });
+
+  const handleRegister = () => {
+    console.log(inputData);
+  };
+
+  const updateField = (fieldName, value) => {
+    setInputData((prev) => ({
+      ...prev,
+      [fieldName]: value,
+    }));
+  };
+
+  return (
+    <Box
+      display="flex"
+      alignItems="flex-start"
+      justifyContent="center"
+      height="100vh"
+      padding="4"
+      pt="32"
+    >
+      <Card.Root width="md">
+        <Card.Header>
+          <Card.Title fontSize="2xl" textAlign="center">
+            Sign up
+          </Card.Title>
+        </Card.Header>
+        <Card.Body>
+          <Stack>
+            {registerForm.fields.map((field, index) => (
+              <LabelledInputField
+                key={index}
+                label={field.label}
+                value={inputData[field.name]}
+                type={field.type}
+                onChange={(e) => updateField(field.name, e.target.value)}
+              />
+            ))}
+          </Stack>
+        </Card.Body>
+        <Card.Footer flexDirection="column" justifyContent="center">
+          <Button width="full" onClick={handleRegister} mb="2">
+            Register
+          </Button>
+          <Card.Description>
+            Already have an account? Sign in{" "}
+            <Text as="span" textDecoration="underline">
+              <Link to="/login">here</Link>
+            </Text>
+          </Card.Description>
+        </Card.Footer>
+      </Card.Root>
+    </Box>
+  );
+}
+
+export default Register;
